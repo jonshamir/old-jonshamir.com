2e75c42765d8aa435ecd8e190a84ee08260ac64b
Jon Shamir <jon.shamir@gmail.com>
2021-06-13 12:15:43 +0300

    Content changes

--- src/Homepage/IntroSection/IntroSection.js
+++ src/Homepage/IntroSection/IntroSection.js
@@ -9,11 +9,15 @@ const IntroSection = (props) => {
         <span id="logoInlinePos"></span>
         <h1>Hi, I'm Jon Shamir</h1>
         <p>
-          I am interested in the intersection of technology, art and design. I
-          believe that design is a powerful tool that has the ability to change
-          perceptions and reveal insights. I enjoy working on projects that
-          combine different ideas & disciplines into something greater than the
-          sum of its parts.
+          Currently based in Jerusalem, I recently graduated from a 5-year
+          program combining computer science & design. My interests lie in the
+          intersection of technology and art and include computer graphics,
+          typography, machine learning and data visualization.
+        </p>
+        <p>
+          I have extensive experience with web technologies and interactive
+          media. I love working on projects that combine different fields and
+          use technologies in new and interesting ways.
         </p>
       </div>
     </div>
