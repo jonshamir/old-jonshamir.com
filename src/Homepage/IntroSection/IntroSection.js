1c17fe86c7955b934ab962dd011d16f289706f79
Jon Shamir <jon.shamir@gmail.com>
2021-07-25 20:29:29 +0300

    Intro changes

--- src/Homepage/IntroSection/IntroSection.js
+++ src/Homepage/IntroSection/IntroSection.js
@@ -10,13 +10,13 @@ const IntroSection = (props) => {
         <h1>Hi, I'm Jon Shamir</h1>
         <p>
           My interests lie at the intersection of technology and visual arts:
-          graphics, typography, machine learning, generative art, data viz and
-          more.
+          computer graphics, machine learning, data visualization and more.
         </p>
         <p>
           I have more than a decade of professional experience with various
-          technologies and mediums. I've previously worked for small and
-          medium-sized startups, as a freelancer and on academic projects.
+          technologies and mediums. Mostly I love working on projects that
+          combine different fields and use technology in new and interesting
+          ways.
         </p>
       </div>
     </div>
